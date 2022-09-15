534f12893a1e48ad5b7ae46c1250bd5c8ce5844b
Lukas Hauser <67017455+lukas-hauser@users.noreply.github.com>
2022-09-15 22:55:47 +0100

    Update plantwatering.py

--- PlantWatering/plantwatering.py
+++ PlantWatering/plantwatering.py
@@ -4,36 +4,13 @@ import secrets
 import network, rp2
 import urequests as requests
 
-led = Pin('LED', Pin.OUT)
-pump1a = Pin(14, Pin.OUT)
-pump1b = Pin(15, Pin.OUT)
-
-rp2.country('UK')
-ssid = secrets.SSID
-password = secrets.PASSWORD
-
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect(ssid, password)
-
-while not wlan.isconnected() and wlan.status() >= 0:
- print("Waiting to connect:")
- time.sleep(1)
-
-print(wlan.ifconfig())
-
-green_led = Pin('LED', Pin.OUT)
-sensor = Pin(17, Pin.IN, Pin.PULL_DOWN)
-sensor_power = Pin(16, Pin.OUT)
-
-def send_notification():
-    if notification_text != '':
-        request_headers = {'Content-Type': 'application/json'}
-        payload = { 'value1':notification_text}
-        ifttt_url = "https://maker.ifttt.com/trigger/thirsty_plant/with/key/"+secrets.IFTTT_KEY
-        request = requests.post(ifttt_url, json=payload, headers=request_headers)
-        request.close()
-
+def send_notification(notification_text):
+    request_headers = {'Content-Type': 'application/json'}
+    payload = { 'value1':notification_text}
+    ifttt_url = "https://maker.ifttt.com/trigger/thirsty_plant/with/key/"+secrets.IFTTT_KEY
+    request = requests.post(ifttt_url, json=payload, headers=request_headers)
+    request.close()
+    
 def save_data():
     request_headers = {'Content-Type': 'application/json'}
     payload = { 'value1':status,'value2':notification_text }
@@ -55,6 +32,29 @@ def add_water():
     pump_on()
     time.sleep(5)
     pump_off()
+    
+led = Pin('LED', Pin.OUT)
+pump1a = Pin(14, Pin.OUT)
+pump1b = Pin(15, Pin.OUT)
+
+rp2.country('UK')
+ssid = secrets.SSID
+password = secrets.PASSWORD
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(ssid, password)
+
+while not wlan.isconnected() and wlan.status() >= 0:
+ print("Waiting to connect:")
+ time.sleep(1)
+
+#print(wlan.ifconfig())
+#send_notification("Thirsty Plant's wlan connected.")
+
+green_led = Pin('LED', Pin.OUT)
+sensor = Pin(17, Pin.IN, Pin.PULL_DOWN)
+sensor_power = Pin(16, Pin.OUT)
 
 status='moist'
 
@@ -80,5 +80,5 @@ while True:
     save_data()
     if status == 'dry':
         add_water()
-    send_notification()
+    send_notification(notification_text)
     time.sleep(3600)
